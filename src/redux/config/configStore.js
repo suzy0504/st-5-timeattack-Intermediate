23fc6daa88d33e21cbb142c3d1ee3761036811aa
unknown <tnwl9540@gmail.com>
2024-06-10 15:36:05 +0900

    calculater 완료

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,1 +1,6 @@
 // TODO: store 셋업하고 slice에서 import한 reducer를 할당하세요.
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+  reducer: { counter: counterReducer },
+});
