ead0660507b40e1bbe8de2847145c50de0963a10
unknown <tnwl9540@gmail.com>
2024-06-10 16:38:21 +0900

    todolist 완료

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 export default function TodoForm() {
+  const [todos, setTodos] = useState("");
+  const [title, setTitle] = useState();
+  // const [content, setContent] = useState();
+  const onChange = () => {
+    setTodos(title);
+  };
+
+  const addHandler = () => {
+    todos(setTitle);
+  };
+
+  console.log(setTitle);
+
   return (
     <form>
       <label>제목: </label>
-      <input type="text" /> <label>내용: </label>
-      <input type="text" />
-      <button>추가</button>
+      <input type="text" value={title} name="title" onChange={onChange} />
+      <label>내용: </label>
+      {/* <input type="text" value={content} onChange={onChange} /> */}
+      <button onClick={addHandler}>추가</button>
     </form>
   );
 }
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,1 +1,12 @@
 // TODO: todoSlice 를 작성하세요.
+import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: {}, //일단제로
+  reducers: {},
+});
+
+export const {} = todoSlice.actions;
+export default todoSlice.reducer;
